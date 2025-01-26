f51b9821540872b0b161422272e4d2817888e278
SynclonSec <161953995+SynclonSec@users.noreply.github.com>
2025-01-26 18:03:27 -0500

    Update types.ts

--- typescriptRaydium/types.ts
+++ typescriptRaydium/types.ts
@@ -1,1 +1,29 @@
+export interface TokenMetadata {
+    mint: string;
+    symbol: string;
+    name: string;
+    decimals: number;
+    logoURI?: string;
+}
 
+export interface PoolInfo {
+    poolAddress: string;
+    tokenA: TokenMetadata & { reserve: string };
+    tokenB: TokenMetadata & { reserve: string };
+    liquidity: {
+        totalSupply: string;
+        openOrdersAddress: string;
+        lpMint: string;
+    };
+    fees: {
+        tradeFeePercentage: string;
+        ownerFeePercentage: string;
+        rawNumerators: {
+            tradeFeeNumerator: number;
+            tradeFeeDenominator: number;
+            ownerFeeNumerator: number;
+            ownerFeeDenominator: number;
+        };
+    };
+    version: number;
+}
